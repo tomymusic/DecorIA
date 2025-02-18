2ae9711f7559640c99825bf27d5cdba6722b0e68
tomymusic <tomasnumber1@gmail.com>
2025-02-18 20:23:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "./components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "./components/ui/card";
+import { Input } from "./components/ui/input";
 import { motion } from "framer-motion";
 import { Loader2, ShoppingCart, Trash } from "lucide-react";
 import ReactBeforeSliderComponent from "react-before-after-slider-component";
