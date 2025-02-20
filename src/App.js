b82e18ce8ab6f7f9db3e1d8ec0e30d6e6d596c07
tomymusic <tomasnumber1@gmail.com>
2025-02-20 02:06:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Button } from "./components/ui/button";
 import { Card, CardContent } from "./components/ui/card";
 import { motion } from "framer-motion";
