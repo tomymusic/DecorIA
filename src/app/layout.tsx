29ece4b396d0e3b41a42a5d7139d80cb1805bb33
tomymusic <tomasnumber1@gmail.com>
2025-02-24 12:14:22 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,21 @@
-import "../styles/globals.css";
+import "./globals.css"; 
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  display: "swap",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es">
+    <html lang="es" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>{children}</body>
     </html>
   );
