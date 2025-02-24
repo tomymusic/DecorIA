8fb59bb7fedd5e8b671fb513f94ff17794cb54a6
tomymusic <tomasnumber1@gmail.com>
2025-02-24 13:04:15 -0300

    Update route.ts

--- src/app/api/redesign-room/route.ts
+++ src/app/api/redesign-room/route.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { redesignRoom } from "@/lib/replicate";
+import { redesignRoom } from "../../lib/replicate";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
